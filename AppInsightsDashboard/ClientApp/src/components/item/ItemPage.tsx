a33e3b262da217c3ab6585974e657daa8468a2d1
Johanne Håøy Horn <johanne.hh@gmail.com>
2020-03-12 09:42:10 +0100

    Use correct item duration for 180 days button

--- AppInsightsDashboard/ClientApp/src/components/item/ItemPage.tsx
+++ AppInsightsDashboard/ClientApp/src/components/item/ItemPage.tsx
@@ -232,7 +232,7 @@ export default class ItemPage extends React.Component<Props, State> {
                         <DurationButton duration={ItemDuration.SevenDays} currentDuration={duration} onClick={() => this.updateDuration(ItemDuration.SevenDays)}>7 days</DurationButton>
                         <DurationButton duration={ItemDuration.ThirtyDays} currentDuration={duration} onClick={() => this.updateDuration(ItemDuration.ThirtyDays)}>30 days</DurationButton>
                         <DurationButton duration={ItemDuration.NinetyDays} currentDuration={duration} onClick={() => this.updateDuration(ItemDuration.NinetyDays)}>90 days</DurationButton>
-                        <DurationButton duration={ItemDuration.NinetyDays} currentDuration={duration} onClick={() => this.updateDuration(ItemDuration.HundredAndEightyDays)}>180 days</DurationButton>
+                        <DurationButton duration={ItemDuration.HundredAndEightyDays} currentDuration={duration} onClick={() => this.updateDuration(ItemDuration.HundredAndEightyDays)}>180 days</DurationButton>
                         {duration === ItemDuration.Custom && 
                             durationFrom != null && 
                             durationTo != null && 
